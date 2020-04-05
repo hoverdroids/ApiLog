9024a4cd7c04410716ebcfd97a55464a235784da
Chris Sprague <spragucm@gmail.com>
2020-04-05 14:19:04 -0400

    Gutted ApiLogDemo as the project

--- Demo/src/androidTest/java/com/hoverdroids/apilog/demo/ExampleInstrumentedTest.kt
+++ Demo/src/androidTest/java/com/hoverdroids/apilog/demo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hoverdroids.apilogdemo", appContext.packageName)
+        assertEquals("com.hoverdroids.apilog.demo", appContext.packageName)
     }
 }
